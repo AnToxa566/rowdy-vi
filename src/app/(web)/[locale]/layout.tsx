36358ef5cdc5055768ed43122bba20f2047a8ab3
Anton <bogachuk566@gmail.com>
2024-08-11 11:21:38 +0300

    perf: gtm is not blocking site

--- src/app/(web)/[locale]/layout.tsx
+++ src/app/(web)/[locale]/layout.tsx
@@ -88,7 +88,7 @@ export default function RootLayout({
         {/* Google Tag Manager */}
         <Script
           id="gtag"
-          strategy="beforeInteractive"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
