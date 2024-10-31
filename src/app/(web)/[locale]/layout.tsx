08b5dd915920694798fa5bbe81dd664d012795f1
Anton <bogachuk566@gmail.com>
2024-10-31 10:24:30 +0200

    perf: paste clarity script

--- src/app/(web)/[locale]/layout.tsx
+++ src/app/(web)/[locale]/layout.tsx
@@ -115,6 +115,19 @@ export default function RootLayout({
           }}
         />
         {/* End Altegio Widget */}
+
+        {/* Start Clarity */}
+        <Script
+          id="clarity"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "or59jxtckc");`,
+          }}
+        />
+        {/* End Clarity */}
       </head>
 
       <body className={montserrat.className}>
