9095dea928a15c2de7bd8425f89db5143e8f0036
Anton <bogachuk566@gmail.com>
2025-01-12 13:06:57 +0200

    chore: redesign feachures cards

--- src/app/(web)/[locale]/components/home-features/feature-card/feature-card.tsx
+++ src/app/(web)/[locale]/components/home-features/feature-card/feature-card.tsx
@@ -14,22 +14,24 @@ export interface FeatureCardProps {
 
 export const FeatureCard: FC<FeatureCardProps> = ({ feature }) => {
   return (
-    <div className="relative z-10 flex items-start gap-2 rounded-xl p-4 transition-shadow shadow-[0px_0px_10px_-2px_#00000040] hover:shadow-[0px_0px_20px_-3px_#00000040]">
-      <span className="text-[#e62621] flex-shrink-0">{feature.icon}</span>
+    <div className="relative overflow-hidden rounded-xl transition-shadow shadow-[0px_0px_12px_3px_#00000040] hover:shadow-[0px_0px_24px_6px_#00000040]">
+      <Image
+        alt="Feature image"
+        src={feature.imageSrc}
+        width={680}
+        height={380}
+        className="absolute z-0 top-0 left-0 w-full h-full object-cover"
+      />
 
-      <div className="flex flex-col gap-4 justify-between h-full">
-        <div className="flex flex-col gap-1">
+      <div className="absolute z-10 top-0 left-0 w-full h-full bg-black opacity-70"></div>
+
+      <div className="relative z-20 p-4 flex gap-2 items-start">
+        <span className="text-[#e62621] flex-shrink-0">{feature.icon}</span>
+
+        <div className="flex flex-col gap-4 text-white">
           <h3 className="text-xl font-semibold">{feature.title}</h3>
-          <p>{feature.description}</p>
+          <p className="font-medium">{feature.description}</p>
         </div>
-
-        <Image
-          alt="Feature image"
-          src={feature.imageSrc}
-          width={680}
-          height={380}
-          className="aspect-[16/9] w-full rounded-lg shadow-lg object-cover"
-        />
       </div>
     </div>
   );
